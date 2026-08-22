43aa51be68d564df824fa481ed6fdf4418f3ff5a
lemans24 <Basil.thomas@live.ca>
2026-08-22 15:41:28 -0400

    Rename intrinsic time strategy workflow structure

--- TomasAI.IFM.Domain.Trade/Strategy/Workflow/Command/Actor/ItiStrategyWorkflowCommandActor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TomasAI.IFM.Domain.Trade.Strategy.IntrinsicTime.Workflow.Command.Actor;
-
-public class ItiStrategyWorkflowCommandActor
-{
-}
